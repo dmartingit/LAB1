1e3790125797f7c6cc97a105e1928898000a3bf9
jeckgit <s72589@htw-dresden.de>
2016-04-14 10:13:13 +0200

    Ja guter change
    weawe

--- LAB1/src/Labyrinth.java
+++ LAB1/src/Labyrinth.java
@@ -4,7 +4,7 @@ public class Labyrinth {
 	private int mLabHeight;
 	
 	public static void main(String[] args) {
-		System.out.println("Hello.");
+		System.out.println("Hello Daniel LOLLOOLOLOOLLOLOLO.");
 	}
 	
 	private void initInterface(int width, int height) {
